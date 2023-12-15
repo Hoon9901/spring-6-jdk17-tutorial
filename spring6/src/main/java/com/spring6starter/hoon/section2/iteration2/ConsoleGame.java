26db8844af11a677c6d3ecadc39877011b9923e6
Hoon9901 <shonn.dev@gmail.com>
2023-12-15 15:24:48 +0900

    new project

---
+++ spring6/src/main/java/com/spring6starter/hoon/section2/iteration2/ConsoleGame.java
@@ -0,0 +1,8 @@
+package com.spring6starter.hoon.section2.iteration2;
+
+public interface ConsoleGame {
+    void up();
+    void down();
+    void left();
+    void right();
+}
---
+++ spring6/src/main/java/com/spring6starter/hoon/section2/iteration3/ConsoleGame.java
@@ -0,0 +1,13 @@
+package com.spring6starter.hoon.section2.iteration3;
+
+public interface ConsoleGame {
+
+    void up();
+
+    void down();
+
+    void left();
+
+    void right();
+
+}
