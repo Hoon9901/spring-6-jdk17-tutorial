f4bff51d7d202652f3c74542307be63f37dd3540
Hoon9901 <shonn.dev@gmail.com>
2023-12-20 15:23:29 +0900

    하나의 Class에서 Spring Context를 실행하는 코드
    - 별도의 Configuration 클래스 없이 Main 함수가 위치한 클래스에서 Bean을 등록한다.

--- spring6-02/src/main/java/com/spring6starter/hoon/section2/GameApplication.java
+++ spring6-02/src/main/java/com/spring6starter/hoon/section2/GameApplication.java
@@ -2,14 +2,41 @@ package com.spring6starter.hoon.section2;
 
 import com.spring6starter.hoon.section2.game.ConsoleGame;
 import com.spring6starter.hoon.section2.game.GameRunner;
+import com.spring6starter.hoon.section2.game.KartGame;
+import com.spring6starter.hoon.section2.game.MarioGame;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
-import java.io.Console;
-
+@Configuration
 public class GameApplication {
+    @Bean
+    @Qualifier("marioGame")
+    public ConsoleGame consoleGame1() {
+        return new MarioGame();
+    }
+
+    @Bean
+    @Primary
+    public ConsoleGame consoleGame2() {
+        return new KartGame();
+    }
+
+    @Bean
+    @Primary
+    public GameRunner gameRunner(ConsoleGame consoleGame) {
+        return new GameRunner(consoleGame);
+    }
+
+    @Bean
+    public GameRunner gameRunner2(@Qualifier("marioGame") ConsoleGame consoleGame) {
+        return new GameRunner(consoleGame);
+    }
 
     public static void main(String[] args) {
-        try (var context = new AnnotationConfigApplicationContext(GameConfiguration.class)) {
+        try (var context = new AnnotationConfigApplicationContext(GameApplication.class)) {
             GameRunner gameRunner = context.getBean(GameRunner.class);
             gameRunner.run();
 
