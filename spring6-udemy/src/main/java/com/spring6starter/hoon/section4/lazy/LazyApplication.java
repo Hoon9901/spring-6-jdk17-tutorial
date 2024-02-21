e95df10601d86af62c7d77feb6265dad84f44772
Hoon9901 <shonn.dev@gmail.com>
2024-02-21 16:21:02 +0900

    지연 초기화와 즉시 초기화

---
+++ spring6-udemy/src/main/java/com/spring6starter/hoon/section4/lazy/LazyApplication.java
@@ -0,0 +1,53 @@
+package com.spring6starter.hoon.section4.lazy;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+
+@Component
+class ClassA {
+
+    public ClassA() {
+        System.out.println("Initalization classA");
+    }
+
+    public void doSomethings() {
+        System.out.println("Hello world!");
+    }
+}
+
+@Component
+@Lazy
+class ClassB {
+    private final ClassA classA;
+
+    public ClassB(ClassA classA) {
+        System.out.println("initalization classB");
+        this.classA = classA;
+    }
+
+    public void run() {
+        classA.doSomethings();
+    }
+}
+
+
+@Configuration
+@ComponentScan
+public class LazyApplication {
+
+    public static void main(String[] args) {
+        try (var context = new AnnotationConfigApplicationContext(LazyApplication.class)) {
+            Arrays.stream(context.getBeanDefinitionNames())
+                    .forEach(System.out::println);
+
+//            context.getBean(ClassA.class).doSomethings();
+            context.getBean(ClassB.class).run();
+        }
+    }
+}
