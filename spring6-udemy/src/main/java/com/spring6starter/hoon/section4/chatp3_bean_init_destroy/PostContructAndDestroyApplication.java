d5981384c73996fbb74388bdf544fc6671272db0
Hoon9901 <shonn.dev@gmail.com>
2024-02-21 17:47:33 +0900

    스프링 Bean 생성 후 초기화, 소멸 이전 작업 기능
    - @PostConstruct, @PreDestroy

---
+++ spring6-udemy/src/main/java/com/spring6starter/hoon/section4/chatp3_bean_init_destroy/PostContructAndDestroyApplication.java
@@ -0,0 +1,50 @@
+package com.spring6starter.hoon.section4.chatp3_bean_init_destroy;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+
+@Component
+class ClassA {
+
+    private final DependencyClass dependencyClass;
+
+    public ClassA(DependencyClass dependencyClass) {
+        this.dependencyClass = dependencyClass;
+        System.out.println("Bean Injection Done");
+    }
+
+    @PostConstruct
+    private void initalize() {
+        System.out.println("Some initalize");
+    }
+
+    @PreDestroy
+    private void cleanup() {
+        System.out.println("Connection Closed or DB CleanUp");
+    }
+}
+
+@Component
+class DependencyClass {
+
+}
+
+@Configuration
+@ComponentScan
+public class PostContructAndDestroyApplication {
+
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(PostContructAndDestroyApplication.class);
+
+        Arrays.stream(context.getBeanDefinitionNames())
+                .forEach(System.out::println);
+
+    }
+}
